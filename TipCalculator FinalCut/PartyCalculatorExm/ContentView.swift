01796732de6c792b0b2b3d546c1c44c51a7b2e38
AntonyCatcher <anton.chvertkov@yahoo.com>
2022-07-11 18:16:06 +0300

    This is a simple Tip Calculator App which allows a company of friends to calculate the total bill and related tips per person after a meal.

---
+++ TipCalculator FinalCut/PartyCalculatorExm/ContentView.swift
@@ -0,0 +1,100 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = 10.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var totalPerPersonWoTip: Double {
+        let peopleCountWoTip = Double(numberOfPeople + 2)
+        
+        let grandTotalWoTip = checkAmount
+        let amountPerPersonWoTip = grandTotalWoTip / peopleCountWoTip
+        
+        return amountPerPersonWoTip
+    }
+    
+    var body: some View {
+        
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format:.currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+            }
+        header: {
+            Text("Enter your bill")
+        }
+            
+            Section {
+                
+                TextField("Number of Guests", value: $numberOfPeople, format: .number)
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+            }
+        header: {
+            Text("Enter number of guests")
+        }
+            
+            Section {
+                Picker("Tip percentage", selection: $tipPercentage) {
+                    ForEach(tipPercentages, id: \.self) {
+                        Text($0, format: .percent)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+        header: {
+            Text("How much tip do you want to leave?")
+        }
+            
+            Section {
+                Text(totalPerPerson, format:.currency(code: Locale.current.currencyCode ?? "USD"))
+            }
+        header: {
+            Text("Your bill with tips")
+        }
+            
+            Section {
+                Text(totalPerPersonWoTip, format:.currency(code: Locale.current.currencyCode ?? "USD"))
+            }
+        header: {
+            Text("Your bill without tips")
+        }
+            
+        }
+        
+        .toolbar{
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                
+                Button("Done") {
+                    amountIsFocused = false
+                }
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
