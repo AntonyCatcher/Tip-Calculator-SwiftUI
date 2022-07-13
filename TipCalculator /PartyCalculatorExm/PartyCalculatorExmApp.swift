386a00e55b0291121153ff808c4b8ee4424d4344
AntonyCatcher <anton.chvertkov@yahoo.com>
2022-07-13 17:01:59 +0300

    TipCalculator

---
+++ TipCalculator /PartyCalculatorExm/PartyCalculatorExmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyCalculatorExmApp.swift
+//  PartyCalculatorExm
+//
+//  Created by Anton  on 08.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PartyCalculatorExmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
